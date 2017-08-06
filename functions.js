7b0a2f2dabaf8e74bb6d1196342a1c7a33ccb0e7
givethroughservice <30480600+givethroughservice@users.noreply.github.com>
2017-08-06 18:29:38 -0500

    Added styling to home page

---
+++ functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+
+  // $('.directorySlider').directorySlider({
+  // 	filebase: 'img_',
+  // 	extension: 'jpg',
+  // 	directory: 'C:/Users/jonat_000/Desktop/Projects/Freelance/GTS/give-through-service/img/',
+  // 	numslides: 7,
+  // 	height: 350
+  // });
+
+
+
+});
