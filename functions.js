df3f7643e7a133d8fe20769873f95865d0c9f38d
givethroughservice <30480600+givethroughservice@users.noreply.github.com>
2017-09-07 16:14:42 -0500

    Fixed slider with Instafeed

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
+
+
+
 $(document).ready(function(){
 
   var scrollTop = 0;
@@ -18,22 +21,42 @@ $(document).ready(function(){
 
   $('#myCarousel').carousel({interval: 7000});
 
-  // Ajax Gallery
-	var folder = "img/";
+  // INSTAFEED
 
-	$.ajax({
-	    url : folder,
-	    success: function (data) {
-	        $(data).find("a").attr("href", function (i, val) {
-	            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-	              $("#myCarousel .carousel-inner").append( "<div class='item'><img src='"+ folder + val +"'></div>" );
-	            	$('#myCarousel .carousel-inner .item:first').addClass('active');
-	            	$('#myCarousel .carousel-indicators').append( '<li data-target="#myCarousel" data-slide-to="' + i + '"></li>' );
-	            	$('#myCarousel .carousel-indicators li:first').addClass('active');
-	            } 
-	        });
-	        $('#myCarousel').carousel('next');
-	    }
-	});
+	var userFeed = new Instafeed({
+    get: 'user',
+    userId: '6005823620',
+    accessToken: '6005823620.1677ed0.20cac08d4a63427888f1c53e5eb6cf13',
+    resolution: 'standard_resolution',
+    sortBy: 'least-recent',
+    template: "<div class='item'><img src='{{image}}' title='{{caption}}'><div class='carousel-caption'><h3>{{caption}}</h3></div></div>",
+    after: function(){
+    	$('#myCarousel .carousel-inner .item:first').addClass('active');
+    	$('#myCarousel .item').each(function(i){
+    		$('.carousel-indicators').append( '<li data-target="#myCarousel" data-slide-to="' + i + '"></li>' );
+    	});
+    	$('#myCarousel .carousel-indicators li:first').addClass('active');
+    }
+  });
+	userFeed.run();
+	
+
+  // Ajax Gallery
+	// var folder = "img/";
+
+	// $.ajax({
+	//     url : folder,
+	//     success: function (data) {
+	//         $(data).find("a").attr("href", function (i, val) {
+	//             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+	//               $("#myCarousel .carousel-inner").append( "<div class='item'><img src='"+ folder + val +"'></div>" );
+	//             	$('#myCarousel .carousel-inner .item:first').addClass('active');
+	//             	$('#myCarousel .carousel-indicators').append( '<li data-target="#myCarousel" data-slide-to="' + i + '"></li>' );
+	//             	$('#myCarousel .carousel-indicators li:first').addClass('active');
+	//             } 
+	//         });
+	//         $('#myCarousel').carousel('next');
+	//     }
+	// });
 
 });
