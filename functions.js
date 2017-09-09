57313879787599f47fcce0095d7c6268ddc02755
givethroughservice <30480600+givethroughservice@users.noreply.github.com>
2017-09-09 16:15:01 -0500

    Added thumbnail navigation to slider

--- functions.js
+++ functions.js
@@ -24,6 +24,7 @@ $(document).ready(function(){
   // INSTAFEED
 
 	var userFeed = new Instafeed({
+		target: 'instafeed1',
     get: 'user',
     userId: '6005823620',
     accessToken: '6005823620.1677ed0.20cac08d4a63427888f1c53e5eb6cf13',
@@ -36,27 +37,97 @@ $(document).ready(function(){
     		$('.carousel-indicators').append( '<li data-target="#myCarousel" data-slide-to="' + i + '"></li>' );
     	});
     	$('#myCarousel .carousel-indicators li:first').addClass('active');
+
+    	// Set height of controls
+    	resizeControls();
     }
   });
-	userFeed.run();
+
+
+
 	
+	var thumbFeed = new Instafeed({
+		target: 'instafeed2',
+    get: 'user',
+    userId: '6005823620',
+    accessToken: '6005823620.1677ed0.20cac08d4a63427888f1c53e5eb6cf13',
+    resolution: 'thumbnail',
+    sortBy: 'least-recent',
+    template: "<li><img src='{{image}}' /></li>",
+		after: function(){
+			;(function(window, $, undefined) {
 
-  // Ajax Gallery
-	// var folder = "img/";
+	var conf = {
+		center: true,
+		backgroundControl: false
+	};
 
-	// $.ajax({
-	//     url : folder,
-	//     success: function (data) {
-	//         $(data).find("a").attr("href", function (i, val) {
-	//             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-	//               $("#myCarousel .carousel-inner").append( "<div class='item'><img src='"+ folder + val +"'></div>" );
-	//             	$('#myCarousel .carousel-inner .item:first').addClass('active');
-	//             	$('#myCarousel .carousel-indicators').append( '<li data-target="#myCarousel" data-slide-to="' + i + '"></li>' );
-	//             	$('#myCarousel .carousel-indicators li:first').addClass('active');
-	//             } 
-	//         });
-	//         $('#myCarousel').carousel('next');
-	//     }
-	// });
+	var cache = {
+		$carouselContainer: $('.thumbnails-carousel').parent(),
+		$thumbnailsLi: $('.thumbnails-carousel li'),
+		$controls: $('.thumbnails-carousel').parent().find('.carousel-control')
+	};
+
+	function init() {
+		cache.$carouselContainer.find('ol.carousel-indicators').addClass('indicators-fix');
+		cache.$thumbnailsLi.first().addClass('active-thumbnail');
+
+		if(!conf.backgroundControl) {
+			cache.$carouselContainer.find('.carousel-control').addClass('controls-background-reset');
+		}
+		else {
+			cache.$controls.height(cache.$carouselContainer.find('.carousel-inner').height());
+		}
+
+		if(conf.center) {
+			cache.$thumbnailsLi.wrapAll("<div class='center clearfix'></div>");
+		}
+	}
+
+	function refreshOpacities(domEl) {
+		cache.$thumbnailsLi.removeClass('active-thumbnail');
+		cache.$thumbnailsLi.eq($(domEl).index()).addClass('active-thumbnail');
+	}	
+
+	function bindUiActions() {
+		cache.$carouselContainer.on('slide.bs.carousel', function(e) {
+  			refreshOpacities(e.relatedTarget);
+		});
+
+		cache.$thumbnailsLi.click(function(){
+			cache.$carouselContainer.carousel($(this).index());
+		});
+	}
+
+	$.fn.thumbnailsCarousel = function(options) {
+		conf = $.extend(conf, options);
+
+		init();
+		bindUiActions();
+
+		return this;
+	}
+
+})(window, jQuery);
+			$('.thumbnails-carousel').thumbnailsCarousel();
+		}
+	});
+  
+	userFeed.run();
+	thumbFeed.run();
+
+
+
+
 
 });
+// .ready();
+
+
+function resizeControls(){
+	var imgHeight = $('#gallery .carousel-inner img').height();
+	imgHeight += 4;
+	$('#gallery #myCarousel .carousel-control').css({
+		height: imgHeight
+	});
+}
